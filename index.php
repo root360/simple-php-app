bc09d5705e93e13e0275de615fc157cc754bef5d
root360 <steffen.drya@root360.de>
2019-07-01 09:20:09 +0200

    Add files via upload
    fixed opening "

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Simple PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href=public/assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="public/assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #ccc;}</style>
-        <link href=public/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link href="public/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
     </head>
