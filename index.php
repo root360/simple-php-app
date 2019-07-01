97e11d0ec544602dd0d46eca8e621bf5ac4acdd1
root360 <steffen.drya@root360.de>
2019-07-01 12:14:49 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src=public/assets/js/bootstrap.min.js"></script>
+        <script src="public/assets/js/bootstrap.min.js"></script>
     </body>
 
 </html>
